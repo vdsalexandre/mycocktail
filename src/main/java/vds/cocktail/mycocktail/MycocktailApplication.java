a6dbae6472af92513f81de5f00c1c496a38e594d
vdsalex <alexandre.vandensteen@gmail.com>
2020-10-25 15:52:01 +0100

    initialize mycocktail project

---
+++ src/main/java/vds/cocktail/mycocktail/MycocktailApplication.java
@@ -0,0 +1,13 @@
+package vds.cocktail.mycocktail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycocktailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycocktailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vds/cocktail/mycocktail/MycocktailApplicationTests.java
@@ -0,0 +1,13 @@
+package vds.cocktail.mycocktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycocktailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
