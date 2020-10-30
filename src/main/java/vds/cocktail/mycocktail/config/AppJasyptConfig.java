848d58870781ea2f657f04d9b63a139fc9d2655d
vdsalex <alexandre.vandensteen@gmail.com>
2020-10-30 23:55:09 +0100

    adding jasypt configuration

---
+++ src/main/java/vds/cocktail/mycocktail/config/AppJasyptConfig.java
@@ -0,0 +1,9 @@
+package vds.cocktail.mycocktail.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "encrypted.properties")
+public class AppJasyptConfig {
+}
