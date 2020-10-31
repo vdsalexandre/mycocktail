6d40c7b454863b5f0f6c6939abfeb87feeafe4a3
vdsalex <alexandre.vandensteen@gmail.com>
2020-10-31 12:41:32 +0100

    refactoring jasypt property service

--- src/main/java/vds/cocktail/mycocktail/config/AppJasyptPropertyService.java
+++ src/main/java/vds/cocktail/mycocktail/config/AppJasyptPropertyService.java
@@ -1,7 +1,6 @@
 package vds.cocktail.mycocktail.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +12,4 @@ public class AppJasyptPropertyService {
     public String getProperty() {
         return property;
     }
-//
-//    public String getDatasourcePasswordUsingEnvironment(Environment environment) {
-//        return environment.getProperty("spring.datasource.password");
-//    }
 }
